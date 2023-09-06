9f851c49207c26e6c7996236d75cb5fc683fda81
yarogono <70641418+yarogono@users.noreply.github.com>
2023-09-06 16:47:39 +0900

    초기 커밋.

---
+++ Assets/Scripts/Managers/Managers.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Managers : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
