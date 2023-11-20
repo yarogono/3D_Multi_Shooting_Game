eafa2a55d07c3ca02162d094e32d8b0a612458ca
yarogono <70641418+yarogono@users.noreply.github.com>
2023-11-21 00:22:21 +0900

    Client/에셋 정리 및 다운로드

---
+++ Common/Packet/bin/GenProtoClient.bat
@@ -0,0 +1,6 @@
+protoc.exe -I=./ --csharp_out=./ ./Protocol.proto
+IF ERRORLEVEL 1 PAUSE
+
+START ../../../Server/PacketGenerator/bin/PacketGenerator.exe ./Protocol.proto
+XCOPY /Y Protocol.cs "../../../Client/Assets/Scripts/Packet"
+XCOPY /Y ClientPacketManager.cs "../../../Client/Assets/Scripts/Packet"
---
+++ Common/Packet/bin/GenProtoDummyClient.bat
@@ -0,0 +1,6 @@
+protoc.exe -I=./ --csharp_out=./ ./Protocol.proto
+IF ERRORLEVEL 1 PAUSE
+
+START ../../../Server/PacketGenerator/bin/PacketGenerator.exe ./Protocol.proto
+XCOPY /Y Protocol.cs "../../../Server/DummyClient/Packet"
+XCOPY /Y ClientPacketManager.cs "../../../Server/DummyClient/Packet"
