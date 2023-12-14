8885157e2e5898741a012f3ee914a0d2bf1fe660
yarogono <70641418+yarogono@users.noreply.github.com>
2023-12-14 18:07:56 +0900

    패킷 수정

--- Common/Packet/bin/GenProtoClient.bat
+++ Common/Packet/bin/GenProtoClient.bat
@@ -1,7 +1,7 @@
 protoc.exe -I=./proto --csharp_out=./ ./proto/*.proto
 IF ERRORLEVEL 1 PAUSE
 
-START ../../../Server/PacketGenerator/bin/PacketGenerator.exe ./proto/Protocol.proto ./proto/Item.proto
+START ../../../Server/PacketGenerator/bin/PacketGenerator.exe ./proto/protocol.proto ./proto/item.proto
 XCOPY /Y Protocol.cs "../../../Client/Assets/Scripts/Packet/Proto"
 XCOPY /Y Item.cs "../../../Client/Assets/Scripts/Packet/Proto"
 XCOPY /Y ClientPacketManager.cs "../../../Client/Assets/Scripts/Packet"
--- Common/Packet/bin/GenProtoServer.bat
+++ Common/Packet/bin/GenProtoServer.bat
@@ -1,7 +1,7 @@
 protoc.exe -I=./proto --csharp_out=./ ./proto/*.proto
 IF ERRORLEVEL 1 PAUSE
 
-START ../../../Server/PacketGenerator/bin/PacketGenerator.exe ./proto/Protocol.proto ./proto/Item.proto
+START ../../../Server/PacketGenerator/bin/PacketGenerator.exe ./proto/protocol.proto ./proto/item.proto
 XCOPY /Y Protocol.cs "../../../Server/Server/Packet/Proto"
 XCOPY /Y Item.cs "../../../Server/Server/Packet/Proto"
 XCOPY /Y ServerPacketManager.cs "../../../Server/Server/Packet"
